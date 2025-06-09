7e0d3d66c4aa16c88e46385dbd9d13730478cc72
mjmandelah07 <aramidemojisolagrace@gmail.com>
2025-06-09 08:09:44 +0100

    updated colors

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from '@/features/auth/store/AuthContext';
 import { router } from '@/shared/routes';
 import '@/shared/global.css';
+import { ThemeProvider } from '@/shared/ThemeContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
--- src/shared/components/ui/ThemeToggle.tsx
+++ src/shared/components/ui/ThemeToggle.tsx
@@ -1,20 +1,17 @@
 import SunIcon from "@/shared/assets/icons/sun.svg?react";
 import MoonIcon from "@/shared/assets/icons/moon.svg?react";
-import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "@/shared/ThemeContext";
 
 export default function ThemeToggle() {
-  const [isDark, setIsDark] = React.useState(false);
-
-  const toggleTheme = () => {
-    setIsDark(!isDark);
-  };
+  const { isDark, toggleTheme } = useContext(ThemeContext);
 
   return (
     <button onClick={toggleTheme} className="w-10 h-10">
       {isDark ? (
         <SunIcon className="w-full h-full object-contain text-yellow-400 drop-shadow" />
       ) : (
-        <MoonIcon className="w-full h-full object-contain text-white drop-shadow" />
+        <MoonIcon className="w-full h-full object-contain  drop-shadow text-[#958C8C]" />
       )}
     </button>
   );
