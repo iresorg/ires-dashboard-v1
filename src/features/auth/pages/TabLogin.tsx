05aa58b26fd19ce0956188fb6e389b377ee1ee28
Atomic Ustadh <ibnluqmanalawfaaweey@gmail.com>
2025-06-15 10:15:59 +0100

    fix-feature

--- src/features/auth/pages/TabLogin.tsx
+++ src/features/auth/pages/TabLogin.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import ThemeToggle from "@shared/components/ui/ThemeToggle";
 import { ThemeProvider } from "@/shared/ThemeContext";
-//import PentagonIcon from "@/shared/assets/icons/pentagon-icon.svg?react";
+import PentagonIcon from "@/shared/assets/icons/pentagon-icon1.svg?react";
 import AdminIcon from "@/shared/assets/icons/admin.png";
 import AgentIcon from "@/shared/assets/icons/agent.svg";
 import ResponderIcon from "@/shared/assets/icons/responder.svg";
@@ -11,6 +11,12 @@ const LOGIN_TYPES = [
   { label: "Admin", value: "admin", icon: AdminIcon },
   { label: "Agent", value: "agent", icon: AgentIcon },
   { label: "Responder", value: "responder", icon: ResponderIcon },
+];
+
+const positions = [
+  "md:relative top-10 right-20",       // Admin
+  "md:relative top-50 left-5",        // Agent
+  "md:relative top-10 left-20",      // Responder
 ];
 
 const TabLogin = () => {
@@ -42,16 +48,18 @@ const TabLogin = () => {
             <ThemeToggle />
           </div>
         </div>
-        <h2 className="text-5xl font-bold mb-6 text-center p-0 m-0 md:text-6xl">WELCOME!</h2>
-        <p className="text-lg font-bold mb-6 text-center md:text-xl">What do you want to login as?</p>
-        {/* Space Here -- Just to unconfused my confused confusion */}
+        <h2 className="text-5xl font-bold mb-6 text-center p-0 m-0 md:text-6xl text-[var(--ires-dark-blue)]">WELCOME!</h2>
+        <p className="text-lg font-bold mb-6 text-center">What do you want to login as?</p>
+
+        {/* Space Here -- Just to unconfuse my confused confusion */}
         <div className="flex flex-col items-center justify-center md:flex-row">
-          {LOGIN_TYPES.map((type) => (
+          {LOGIN_TYPES.map((type, idx) => (
             <button
               key={type.value}
-              className="pentagon-btn bg-[var(--ires-white)] text-[var(--ires-black)] flex flex-col items-center justify-center w-35 h-35 mb-5 mx-1 border-2 font-semibold transition-colors duration-200 text-sm"
+              className={`pentagon-btn bg-transparent text-[var(--ires-black)] flex flex-col items-center justify-center w-30 h-30 mb-3 mx-1 md:w-40 md:h-40 font-semibold transition-colors duration-200 hover:bg-[var(--ires-red)] text-sm ${positions[idx]}`}
               onClick={() => handleLoginTypeClick(type.value)}
               type="button">
+              <PentagonIcon className="absolute inset-0 w-full h-full hover:bg-[var(--ires-red)]" />
               <img src={type.icon} alt={type.label} className="w-7 h-7 mb-2" />
               {type.label}
             </button>
