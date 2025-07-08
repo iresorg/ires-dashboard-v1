896dfb015650d87c940f20b452597707e21aaa81
Atomic Ustadh <ibnluqmanalawfaaweey@gmail.com>
2025-07-08 14:49:45 +0100

    Add-Admin-Modal --> labels
    text-right

--- src/features/admin/components/AddAdminModal.tsx
+++ src/features/admin/components/AddAdminModal.tsx
@@ -47,26 +47,26 @@ const AddAdminModal: React.FC<AddAdminModalProps> = ({ onClose }) => {
 
         <form className="space-y-5">
           {/* First Name */}
-          <div className="flex flex-row">
-            <label className="text-gray-700 mb-1 w-1/3">First Name</label>
+          <div className="flex flex-row space-x-3">
+            <label className="text-gray-700 mb-1 w-1/3 text-right">First Name</label>
             <input type="text" className="w-full rounded-lg bg-gray-200 px-4 py-2 focus:outline-none" placeholder="" />
           </div>
 
           {/* Last Name */}
-          <div className="flex flex-row">
-            <label className="text-gray-700 mb-1 w-1/3">Last Name</label>
+          <div className="flex flex-row space-x-3">
+            <label className="text-gray-700 mb-1 w-1/3 text-right">Last Name</label>
             <input type="text" className="w-full rounded-lg bg-gray-200 px-4 py-2 focus:outline-none" placeholder="" />
           </div>
 
           {/* Email */}
-          <div className="flex flex-row">
-            <label className="text-gray-700 mb-1 w-1/3">Email</label>
+          <div className="flex flex-row space-x-3">
+            <label className="text-gray-700 mb-1 w-1/3 text-right">Email</label>
             <input type="email" className="w-full rounded-lg bg-gray-200 px-4 py-2 focus:outline-none" placeholder="" />
           </div>
 
           {/* Role Dropdown */}
-          <div className="flex flex-row">
-            <label className="text-gray-700 mb-1 w-1/3">Role</label>
+          <div className="flex flex-row space-x-2">
+            <label className="text-gray-700 mb-1 w-1/3 text-right">Role</label>
             <select className="w-full rounded-lg bg-gray-200 px-4 py-2 focus:outline-none appearance-none">
               <option value="" disabled selected className="hidden">-Select-</option>
               <option className="bg-white">Agent Admin</option>
