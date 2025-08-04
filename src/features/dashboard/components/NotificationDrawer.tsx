e6bbf3698effed3006952d08863ddf1c35ecfe17
Atomic Ustadh <ibnluqmanalawfaaweey@gmail.com>
2025-08-04 05:00:31 +0100

    UI:: Fix
    Monday, 4 August, 2025

--- src/features/dashboard/components/NotificationDrawer.tsx
+++ src/features/dashboard/components/NotificationDrawer.tsx
@@ -31,12 +31,12 @@ const NotificationDrawer: React.FC<NotificationDrawerProps> = ({
 
       {/* Drawer content */}
       <div
-        className="absolute top-[70px] bottom-0 left-[65%] z-[60] flex items-start justify-end"
+        className="absolute top-[70px] bottom-0 left-[40%] z-[60] flex items-start justify-end"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="bg-white rounded-xl shadow-xl w-[350px]">
+        <div className="bg-[#F9FAFB] rounded-xl shadow-xl w-[350px]">
           {/* Header */}
-          <div className="w-full flex justify-between items-center py-2 border-b-4 border-[#B27373] mb-6">
+          <div className="w-full flex justify-between items-center py-2 border-b-3 !border-b-[#D9D9D9] mb-6">
             <h2 className="bg-[#12096f] text-white px-5 py-1 rounded-xl ml-3">
               Notifications
             </h2>
@@ -50,34 +50,10 @@ const NotificationDrawer: React.FC<NotificationDrawerProps> = ({
           </div>
 
           {/* Content */}
-          <div className="space-y-3 w-auto mx-5 border border-gray-100 px-5 py-8 rounded-xl mb-10">
-            <h2 className="w-60 text-md font-semibold">
-              You have 4 Notifications
+          <div className="bg-[#FCF9F9] space-y-3 w-auto mx-10 border border-gray-100 px-5 py-30 rounded-xl my-30">
+            <h2 className="w-60 text-md font-semibold text-center">
+              No Notifications Yet
             </h2>
-
-            <div className="flex flex-row items-center space-x-1">
-              <div className="w-full px-3 py-2 bg-gray-300 text-gray-900 rounded-lg">
-                New ticket assigned to you.
-              </div>
-            </div>
-
-            <div className="flex flex-row items-center space-x-1">
-              <div className="w-full px-3 py-2 bg-gray-300 text-gray-900 rounded-lg">
-                Password reset successful.
-              </div>
-            </div>
-
-            <div className="flex flex-row items-center space-x-1">
-              <div className="w-full px-3 py-2 bg-gray-300 text-gray-900 rounded-lg">
-                New responder added.
-              </div>
-            </div>
-
-            <div className="flex flex-row items-center justify-start space-x-1">
-              <div className="w-full px-3 py-2 bg-gray-300 text-gray-900 rounded-lg">
-                New agent added.
-              </div>
-            </div>
           </div>
         </div>
       </div>
